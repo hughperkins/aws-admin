e665b1d1d934eed140ffa6712db75f817a5aa8b2
Hugh Perkins <hughperkins@gmail.com>
2016-05-14 22:20:37 +0100

    checkinst2.py location indepdentn

--- checkinst2.py
+++ checkinst2.py
@@ -2,11 +2,15 @@ from __future__ import print_function, unicode_literals
 
 import sys
 import os
+from os import path
+from os.path import join
 import os.path
 import yaml
 import json
 import subprocess
 
+script_dir = path.dirname(path.realpath(__file__))
+# print('script_dir', script_dir)
 aws_path = sys.executable.replace('python', 'aws')
 contents = subprocess.check_output([
   aws_path,
@@ -26,7 +30,7 @@ for res in data['Reservations']:
     instanceInfo['type'] = str(inst['InstanceType'])
     instanceInfo['publicip'] = str(inst['PublicIpAddress'])
     instanceInfo['name'] = instanceInfo['publicip']
-    for tag in inst['Tags']:
+    for tag in inst.get('Tags', []):
       if str(tag['Key']) == 'Name':
         instanceInfo['name'] = str(tag['Value'])
     instanceInfos.append( instanceInfo )
@@ -34,8 +38,8 @@ for res in data['Reservations']:
 newString = yaml.safe_dump(instanceInfos, default_flow_style = False )
 
 oldString = ''
-if os.path.isfile('oldlines.txt'):
-    f = open('oldlines.txt','r')
+if os.path.isfile(join(script_dir, 'oldlines.txt')):
+    f = open(join(script_dir, 'oldlines.txt'),'r')
     oldString = f.read()
     f.close()
 
@@ -44,8 +48,8 @@ if(newString != oldString):
    print( oldString )
    print('new:')
    print( newString )
-   f = open('~oldlines.txt', 'w')
+   f = open(join(script_dir, '~oldlines.txt'), 'w')
    f.write( newString )
    f.close()
-   os.rename( '~oldlines.txt', 'oldlines.txt' )
+   os.rename(join(script_dir, '~oldlines.txt'), join(script_dir, 'oldlines.txt' ))
 
