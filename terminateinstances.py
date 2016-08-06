51c8421c8b9717e7f3568d3570323933efff99b2
Hugh Perkins <hughperkins@gmail.com>
2016-08-06 22:03:56 +0800

    pep8, and first try stopping intance

--- terminateinstances.py
+++ terminateinstances.py
@@ -20,36 +20,54 @@ script_dir = path.dirname(path.realpath(__file__))
 aws_path = sys.executable.replace('python', 'aws')
 
 with open(join(conf_dir, 'allowed_instances.yaml'), 'r') as f:
-  allowed_instances = set(yaml.load(f))
-#print('allowed_instances', allowed_instances)
+    allowed_instances = set(yaml.load(f))
+# print('allowed_instances', allowed_instances)
 
-regions = 'us-west-1 us-west-2 ap-southeast-1 us-east-1 eu-west-1 sa-east-1 ap-southeast-2 ap-northeast-1'.split(' ')
-#print('regions', regions)
+regions = [
+    'us-west-1',
+    'us-west-2',
+    'us-east-1',
+    'eu-west-1',
+    'sa-east-1',
+    'ap-northeast-1',
+    'ap-southeast-1',
+    'ap-southeast-2',
+]
+# print('regions', regions)
 
 instances = []
 for region in regions:
-#    print('region', region)
+    # print('region', region)
     contents = subprocess.check_output([
-      aws_path,
-      '--region', region,
-      'ec2',
-      'describe-instances',
-      '--filters', 'Name=instance-state-code,Values=16'
+        aws_path,
+        '--region', region,
+        'ec2',
+        'describe-instances',
+        '--filters', 'Name=instance-state-code,Values=16'
     ])
     data = json.loads(contents)
-
     for res in data['Reservations']:
-      for inst in res['Instances']:
-        if inst.get('InstanceLifecycle', '') == 'spot':
-          continue
-        instanceId = str(inst['InstanceId'])
-        if instanceId not in allowed_instances:
-          print('terminating', instanceId)
-          print(subprocess.check_output([
-            aws_path,
-            '--region', region,
-            'ec2',
-            'terminate-instances',
-            '--instance-ids', instanceId
-          ]))
-
+        for inst in res['Instances']:
+            if inst.get('InstanceLifecycle', '') == 'spot':
+                continue
+            instanceId = str(inst['InstanceId'])
+            if instanceId not in allowed_instances:
+                try:
+                    print('stopping', instanceId)
+                    print(subprocess.check_output([
+                        aws_path,
+                        '--region', region,
+                        'ec2',
+                        'stop-instances',
+                        '--instance-ids', instanceId
+                    ]))
+                    print('terminating', instanceId)
+                    print(subprocess.check_output([
+                        aws_path,
+                        '--region', region,
+                        'ec2',
+                        'terminate-instances',
+                        '--instance-ids', instanceId
+                    ]))
+                except Exception as e:
+                    print('exception: ', e)
