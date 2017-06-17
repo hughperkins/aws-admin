fe6ea604db1567091e16d3af7ac4eae65b1e71da
Hugh Perkins <hughperkins@gmail.com>
2017-06-17 09:15:45 +0100

    dont forget to exit

--- win/maybe_shutdown.py
+++ win/maybe_shutdown.py
@@ -1,6 +1,7 @@
 import os
 from os import path
 import subprocess
+import sys
 
 
 flag_file = 'c:\\dontshutdown.flag'
@@ -8,6 +9,7 @@ flag_file = 'c:\\dontshutdown.flag'
 if path.isfile(flag_file):
     os.unlink(flag_file)
     print('removed %s. not shutting down' % flag_file)
+    sys.exit(0)
 
 print('shutting down...')
 print(subprocess.check_output([
