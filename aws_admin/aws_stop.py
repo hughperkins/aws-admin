5b9eda8fd5e080ebbdf3656ebeb22ffb65c0b1ac
Hugh Perkins <hughperkins@gmail.com>
2025-02-16 06:16:14 -0500

    add -w --wait to aws_stop

--- aws_admin/aws_stop.py
+++ aws_admin/aws_stop.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+import sys
+import time
 import boto3
 import os
 import argparse
@@ -6,12 +8,7 @@ from colorama import init as colorama_init
 from aws_admin import aws_common
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--profile', default=os.environ.get('AWS_PROFILE', 'default'))
-    parser.add_argument('--region', '-r', default=os.environ.get('AWS_REGION', 'virginia'))
-    parser.add_argument('--name', '-n', required=True)
-    args = parser.parse_args()
+def stop(args):
 
     region_code = aws_common.region_code_by_name[args.region]
     print('region_code', region_code)
@@ -24,3 +21,26 @@ if __name__ == '__main__':
     instance_id = instance['InstanceId']
     print(instance_id)
     ec2.stop_instances(InstanceIds=[instance_id])
+
+    if not args.wait:
+        return
+    
+    instance = aws_common.get_instance(ec2=ec2, name=args.name)
+    state = instance['State']['Name']
+    while state != 'stopped':
+        print(state, file=sys.stderr)
+        time.sleep(3)
+        instance = aws_common.get_instance(ec2=ec2, name=args.name)
+        state = instance['State']['Name']
+    print('stopped', file=sys.stderr)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--profile', default=os.environ.get('AWS_PROFILE', 'default'))
+    parser.add_argument('--region', '-r', default=os.environ.get('AWS_REGION', 'virginia'))
+    parser.add_argument('--name', '-n', required=True)
+    parser.add_argument('--wait', '-w', action='store_true')
+    args = parser.parse_args()
+
+    stop(args)
